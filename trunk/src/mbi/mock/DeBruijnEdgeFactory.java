98b31197479f755ec479ef20612f18a5d24ac0d9
tomek.bawej <tomek.bawej@39ef4c27-136f-87bd-d9d6-2eddcb7760c0>
2012-06-24 13:31:38 +0000

    just crappy demos added

---
+++ trunk/src/mbi/mock/DeBruijnEdgeFactory.java
@@ -0,0 +1,16 @@
+package mbi.mock;
+
+import org.jgrapht.EdgeFactory;
+
+public class DeBruijnEdgeFactory implements EdgeFactory<String, String> {
+
+	@Override
+	public String createEdge(String v1, String v2) {
+		if(v1.substring(1).equals(v2.substring(0, v2.length()-1))){
+			return v1.substring(1);
+		}else{
+			return null;
+		}
+	}
+
+}
